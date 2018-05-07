1b9d203e5834892d7519da8b09097683580a3ae5
Bruno Melo <brmelo86@hotmail.com>
2018-05-07 15:34:41 -0300

    Cadastro_Senha methods

--- features/Android/screens/cadastro_senha.rb
+++ features/Android/screens/cadastro_senha.rb
@@ -2,4 +2,16 @@ class Cadastro_Senha <BaseScreen
     def screen 
         name_screen_id ('linearLayoutPassword')
     end
+
+    def send field_id, text
+        send_keys_id field_id, text
+    end
+
+    def click (bt)
+        button_click_id (bt)
+    end
+
+    def find_id (id)
+        find_element_id (id)
+    end
 end
