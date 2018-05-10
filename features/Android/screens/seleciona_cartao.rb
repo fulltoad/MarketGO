c5c6fd581c74c8d98f970d75ef774a02084c305f
Bruno Melo <brmelo86@hotmail.com>
2018-05-10 15:59:58 -0300

    Rename file

---
+++ features/Android/screens/seleciona_cartao.rb
@@ -0,0 +1,13 @@
+class Seleciona_Cartao < BaseScreen
+    def screen
+        name_screen_id 'linearLayoutSelectCard'
+    end
+
+    def click id
+        button_click_id (id)
+    end
+
+end
+
+
+#bt_sair = menu_card_phone
